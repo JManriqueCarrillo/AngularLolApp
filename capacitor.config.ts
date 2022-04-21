764ad247acea89aad2f567eecc566b35f551a818
JManrique <jose.manrique.carrillo@gmail.com>
2022-04-21 15:12:39 +0200

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LolDataProject',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
